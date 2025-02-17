f68bf1e6826ffcfbd68fa14422dd98e940d4e3a9
nur-alam <nuralam862@gmail.com>
2025-02-17 19:23:07 +0600

    formate while loop in index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ get_header();
 			while ( have_posts() ) :
 				the_post();
 				get_template_part( 'template-parts/content', get_post_type() );
-		endwhile;
+			endwhile;
 			the_posts_navigation();
 		else :
 			get_template_part( 'template-parts/content', 'none' );
