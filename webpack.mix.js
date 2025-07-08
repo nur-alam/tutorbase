14d5f3afe3c7152170846f7bb1fc9aa316e7c5da
nur-alam <nuralam862@gmail.com>
2025-07-08 13:28:15 +0600

    Remove project configuration files and assets for and rename to tutorbase

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.setPublicPath('./assets/dist');
-
-mix.js('assets/src/scripts/app.js', 'assets/dist/js/app.min.js').sass(
-	'assets/src/sass/style.scss',
-	'assets/dist/css/style.min.css'
-);
